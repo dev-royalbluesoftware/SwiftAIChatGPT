6a22f53aea6e4616ea5b2f35eec0973e1db67657
rbs-dev <111300320+dev-royalbluesoftware@users.noreply.github.com>
2025-05-10 12:41:09 -0600

    Initial Commit

---
+++ SwiftAIChatGPT/SwiftAIChatGPTApp.swift
@@ -0,0 +1,20 @@
+//
+//
+// SwiftAIChatGPT
+// SwiftAIChatGPTApp.swift
+//
+// Created by rbs-dev
+// Copyright © Royal Blue Software
+// 
+
+
+import SwiftUI
+
+@main
+struct SwiftAIChatGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
